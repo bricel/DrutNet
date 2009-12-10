458b9116459089261cfbcc84ab89e23d6c15555d
U-Brice-PC\Brice <Brice@Brice-PC.(none)>
2009-12-10 10:46:29 +0200

    First Commit

---
+++ SimplePDM_API/Forms/LoadDialog.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SimplePDM
+{
+    partial class LoadContentDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSaveStyle = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openStyleInBrowserAfterSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.promptOverwriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.useNodeIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.useStyleTitleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbLoadNodeID = new System.Windows.Forms.TextBox();
+            this.txbMessages = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Style Title";
+            // 
+            // buttonSaveStyle
+            // 
+            this.buttonSaveStyle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveStyle.Location = new System.Drawing.Point(133, 83);
+            this.buttonSaveStyle.Name = "buttonSaveStyle";
+            this.buttonSaveStyle.Size = new System.Drawing.Size(100, 22);
+            this.buttonSaveStyle.TabIndex = 3;
+            this.buttonSaveStyle.Text = "Load";
+            this.buttonSaveStyle.UseVisualStyleBackColor = true;
+            this.buttonSaveStyle.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(246, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openStyleInBrowserAfterSaveToolStripMenuItem,
+            this.promptOverwriteToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.useNodeIDToolStripMenuItem,
+            this.useStyleTitleToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // openStyleInBrowserAfterSaveToolStripMenuItem
+            // 
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.Checked = true;
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.CheckOnClick = true;
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.Name = "openStyleInBrowserAfterSaveToolStripMenuItem";
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.openStyleInBrowserAfterSaveToolStripMenuItem.Text = "Open Content in Browser";
+            // 
+            // promptOverwriteToolStripMenuItem
+            // 
+            this.promptOverwriteToolStripMenuItem.Checked = true;
+            this.promptOverwriteToolStripMenuItem.CheckOnClick = true;
+            this.promptOverwriteToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.promptOverwriteToolStripMenuItem.Name = "promptOverwriteToolStripMenuItem";
+            this.promptOverwriteToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.promptOverwriteToolStripMenuItem.Text = "Prompt Overwrite";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(204, 6);
+            // 
+            // useNodeIDToolStripMenuItem
+            // 
+            this.useNodeIDToolStripMenuItem.Name = "useNodeIDToolStripMenuItem";
+            this.useNodeIDToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.useNodeIDToolStripMenuItem.Text = "Use Node ID";
+            this.useNodeIDToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // useStyleTitleToolStripMenuItem
+            // 
+            this.useStyleTitleToolStripMenuItem.Checked = true;
+            this.useStyleTitleToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.useStyleTitleToolStripMenuItem.Name = "useStyleTitleToolStripMenuItem";
+            this.useStyleTitleToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.useStyleTitleToolStripMenuItem.Text = "Use Content Title";
+            this.useStyleTitleToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpPageToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpPageToolStripMenuItem
+            // 
+            this.helpPageToolStripMenuItem.Name = "helpPageToolStripMenuItem";
+            this.helpPageToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.helpPageToolStripMenuItem.Text = "Help Page";
+            this.helpPageToolStripMenuItem.Click += new System.EventHandler(this.helpPageToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Group";
+            // 
+            // txbLoadNodeID
+            // 
+            this.txbLoadNodeID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbLoadNodeID.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txbLoadNodeID.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txbLoadNodeID.Location = new System.Drawing.Point(79, 27);
+            this.txbLoadNodeID.Name = "txbLoadNodeID";
+            this.txbLoadNodeID.Size = new System.Drawing.Size(155, 20);
+            this.txbLoadNodeID.TabIndex = 10;
+            // 
+            // txbMessages
+            // 
+            this.txbMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbMessages.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbMessages.Location = new System.Drawing.Point(8, 111);
+            this.txbMessages.Multiline = true;
+            this.txbMessages.Name = "txbMessages";
+            this.txbMessages.ReadOnly = true;
+            this.txbMessages.Size = new System.Drawing.Size(226, 53);
+            this.txbMessages.TabIndex = 8;
+            // 
+            // LoadContentDialog
+            // 
+            this.AcceptButton = this.buttonSaveStyle;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(246, 176);
+            this.Controls.Add(this.txbLoadNodeID);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbMessages);
+            this.Controls.Add(this.buttonSaveStyle);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(262, 214);
+            this.Name = "LoadContentDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SimplePDM Bridge";
+            this.Shown += new System.EventHandler(this.SaveLoadContentDialog_Shown);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoadContentDialog_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSaveStyle;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openStyleInBrowserAfterSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem useNodeIDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem useStyleTitleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpPageToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbLoadNodeID;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem promptOverwriteToolStripMenuItem;
+        private System.Windows.Forms.TextBox txbMessages;
+    }
+}
---
+++ SimplePDM_API/Forms/LoadDialog.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using SimplePDM;
+using System.Threading;
+using System.IO;
+
+namespace SimplePDM
+{
+    public partial class LoadContentDialog : BaseForm
+    {
+        SimplePDMContent _content;
+        SimplePDMUser _user;
+        Enums.ContentType _contentType = Enums.ContentType.Undefined;
+        AboutBoxSimplePDM aboutBox = new AboutBoxSimplePDM();
+        string ContentString
+        {
+            get
+            {
+                if (_contentType == Enums.ContentType.Undefined)
+                    return "";
+                else
+                    return _contentType.ToString();
+            }
+        }
+        string actionString = "";
+        string _errorMsg = "";
+        bool _PDSplugin = false;
+        string _localFolder = Enums.DOCUMENTFOLDER;
+        bool _v10_2 = false;
+        string _autoCompleteFile = Enums.DOCUMENTFOLDER + "autocomplete.txt";
+        public SimplePDMContent ContentRef
+        {
+            get { return _content; }
+        }
+        /// <summary>
+        /// Display a dialog with save option for the content
+        /// </summary>
+        /// <param name="content">Content type to save/load, will return the save/loaded as object too</param>
+        /// <param name="saveForm">define if the dialog act as save or load</param>
+        public LoadContentDialog(bool V10_2,bool pdsPlugin)
+        {
+            InitializeComponent();
+            
+            actionString = "Loading";
+
+            SimplePDM.BaseSimplePDM.OnUpdateLog += new SimplePDM.BaseSimplePDM.UpdateLog(writeMessage);
+           // txbLoadNodeID.AutoCompleteCustomSource.AddRange(BaseSimplePDM.ReadAutoCompleteFile(_autoCompleteFile));
+            _v10_2 = V10_2;
+            _PDSplugin = pdsPlugin;
+            BaseSimplePDM.OnSubTaskDone += new BaseSimplePDM.SubTaskDoneDel(BaseSimplePDM_OnSubTaskDone);
+            if (!pdsPlugin)
+                this.ShowInTaskbar = true;
+            else
+                this.ShowInTaskbar = false;
+
+
+        }
+        public bool DisplayLoadDialog(SimplePDMUser user, Enums.ContentType contentType, string folderPath)
+        {
+            if (!_PDSplugin)
+                this.StartPosition = FormStartPosition.CenterScreen;
+            _user = user;
+            _contentType = contentType;
+            _localFolder = folderPath;
+            return (this.ShowDialog() == DialogResult.OK);
+        }
+        #region user interface methods
+        private void SaveLoadContentDialog_Shown(object sender, EventArgs e)
+        {
+            Settings.Reload();
+            txbLoadNodeID.AutoCompleteCustomSource.Clear();
+            txbLoadNodeID.AutoCompleteCustomSource.AddRange(BaseSimplePDM.ReadAutoCompleteFile(_autoCompleteFile));
+            openStyleInBrowserAfterSaveToolStripMenuItem.Checked = Settings.OpenContInBrowser;
+            promptOverwriteToolStripMenuItem.Checked = Settings.PromptOverWrite;
+            useNodeIDToolStripMenuItem.Checked = Settings.UseNodeD;
+            useStyleTitleToolStripMenuItem.Checked = Settings.UseContTitle;
+
+            buttonSaveStyle.Text = "Load " + ContentString;//move load button up
+            updateLabel1(false);
+
+            txbMessages.Text = "";
+            this.txbLoadNodeID.Text = "";
+            writeLocalLog("");
+            //display group
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(OrganicGroup.AdminOrganicGroupsArray(_user.OrganicGroups));
+            comboBox1.DisplayMember = "Name";
+            if ((comboBox1.SelectedItem == null) && (comboBox1.Items.Count > 0))
+            {
+                comboBox1.SelectedIndex = 0;
+                //_user.OrganicGroups[0].LoadVocabInListView(listViewTags);//moved to group selection event changed 
+            }
+            this.buttonSaveStyle.Enabled = true;
+            this.txbLoadNodeID.Focus();
+
+        }
+        private void updateLabel1(bool switchState)
+        {
+            if (switchState)
+            {
+                useNodeIDToolStripMenuItem.Checked = !useNodeIDToolStripMenuItem.Checked;
+                useStyleTitleToolStripMenuItem.Checked = !useStyleTitleToolStripMenuItem.Checked;
+            }
+            if (useStyleTitleToolStripMenuItem.Checked)
+            {
+                label1.Text = ContentString + " Title";
+                comboBox1.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "Node ID";
+                comboBox1.Enabled = false;
+            }
+        }
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            updateLabel1(true);
+        }
+        delegate void writeLogCallback(string text);
+        void writeLocalLog(string msg)
+        {
+            //Dealing with threads 
+            if (this.InvokeRequired)
+            {
+                writeLogCallback d = new writeLogCallback(writeLocalLog);
+                this.Invoke(d, new object[] { msg });
+            }
+            else
+            {
+                txbMessages.Text = msg;// +"\n";
+            }
+        }
+        void writeMessage(string str, Enum mSender, Enums.MessageType mType)
+        {
+            if ((mType == Enums.MessageType.Error))
+            {
+                _errorMsg = str;
+                writeLocalLog(str);
+            }
+        }
+        #endregion
+        void BaseSimplePDM_OnSubTaskDone(object task, Enums.SubTaskName subTask)
+        {
+            if (subTask == Enums.SubTaskName.LoadSaveDialogSaveDone)
+                if ((bool) task)
+                     this.DialogResult = DialogResult.OK;//closes the dialog too
+            buttonSaveStyle.Enabled = true;
+
+        }
+        private void buttonLoadTags_Click(object sender, EventArgs e)
+        {
+            //load of current status of existing selected tags
+            if (validateTitleNid(txbLoadNodeID.Text))
+                _content = loadContentInfo(_contentType, _user, (OrganicGroup)comboBox1.SelectedItem, txbLoadNodeID.Text);
+        }
+        /// <summary>
+        /// Check if title or id is not empty and display an error message when needed
+        /// </summary>
+        /// <returns></returns>
+        private bool validateTitleNid(string txbContent)
+        {
+            if (txbContent == "") //nothing in textbox
+            {
+                if (useStyleTitleToolStripMenuItem.Checked)
+                    _errorMsg = "Please type a " + ContentString + " title ";
+                else
+                    _errorMsg = "Please type a node ID";
+                return false;
+            }
+            else
+            {
+                if (useNodeIDToolStripMenuItem.Checked)
+                {//check numbers were entered
+                    try
+                    {
+                        Convert.ToInt32(txbContent);
+                    }
+                    catch
+                    {
+                        _errorMsg = "Please type a correct node ID";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// load the content matching the title/nid
+        /// </summary>
+        private SimplePDMContent loadContentInfo(Enums.ContentType cType, SimplePDMUser user, OrganicGroup group, string txbContent)
+        {
+            // ****** use node ID Load/Save *********
+            SimplePDMContent content = null;
+            if (useNodeIDToolStripMenuItem.Checked)
+            {
+                content = new SimplePDMContent(cType, user);
+                content.NodeID = Convert.ToInt32(txbContent);
+            }
+            // ****** use  title Load/Save *********
+            else
+            {
+                //seach for title name
+                List<SimplePDMContent> searchResult = SimplePDMContent.SearchContent(txbContent, group, cType, user);
+                if (searchResult.Count == 0)//no result found
+                    _errorMsg = "This " + ContentString + " Title doesn't exist. Try with a different Title or Group.";
+                else
+                    //TODO: handle multiple search result
+                    content = searchResult[0];//take first choise
+            }
+            return content;
+        }
+        private void runSaveThread(string txbContent, OrganicGroup group)
+        {
+            writeLocalLog("");
+            bool actionRes = false;
+            if (validateTitleNid(txbContent))
+            {
+                writeLocalLog(actionString + "...");
+                try
+                {
+                    _content = loadContentInfo(_contentType, _user, group, txbContent);
+                    //********************* update/load actual actions **********************
+                    if (_content != null)
+                    {
+
+                        actionRes = _content.Load(_localFolder, true, true, promptOverwriteToolStripMenuItem.Checked, this);
+                        if (!((_contentType == Enums.ContentType.Undefined) || (_contentType == _content.ContentType)))
+                        {
+                            actionRes = false;
+                            _errorMsg = "Content type requested is not a " + ContentString;
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    writeLocalLog(ex.Message);
+                }
+            } // of  load
+            if ((actionRes)&&(_content!=null))
+            {
+                if (openStyleInBrowserAfterSaveToolStripMenuItem.Checked)
+                    _content.OpenContentWebPage(Enums.OpenWebPageMode.View);
+            }
+            else
+            {
+              //  if (txbMessages.Text == actionString + "...")
+                
+                if (_errorMsg != "")
+                    writeLocalLog(_errorMsg);
+                else
+                    writeLocalLog("Error " + this.actionString);
+
+            }
+
+            BaseSimplePDM.SubTaskDone(actionRes, Enums.SubTaskName.LoadSaveDialogSaveDone);
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            txbMessages.Text = "";
+            buttonSaveStyle.Enabled = false;
+            if (!txbLoadNodeID.AutoCompleteCustomSource.Contains(txbLoadNodeID.Text))//add new value to autocomplete list
+            {
+                BaseSimplePDM.WriteAutoCompleteFile(txbLoadNodeID.Text.Trim(), _autoCompleteFile);
+                txbLoadNodeID.AutoCompleteCustomSource.Add(txbLoadNodeID.Text.Trim());
+            }
+            if (!_v10_2)
+            {
+                Thread saveThread = new Thread(delegate()
+                { runSaveThread(txbLoadNodeID.Text.Trim(), (OrganicGroup)comboBox1.SelectedItem); });
+                saveThread.Start();
+            }
+            else
+                runSaveThread(txbLoadNodeID.Text.Trim(), (OrganicGroup)comboBox1.SelectedItem);
+        }
+
+        private void helpPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this._PDSplugin)
+                 BaseSimplePDM.OpenHelpPage("SimplePDM_Bridge_Plugin_For_OptiTex_PDS");
+            else
+                BaseSimplePDM.OpenHelpPage("SimplePDM_Bridge_Plugin_For_OptiTex_PDS");
+
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aboutBox.ShowDialog(this);
+        }
+
+        private void LoadContentDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            base.SaveOpenSaveOptions(
+                  promptOverwriteToolStripMenuItem.Checked,
+                  openStyleInBrowserAfterSaveToolStripMenuItem.Checked,
+                  useStyleTitleToolStripMenuItem.Checked,
+                  useNodeIDToolStripMenuItem.Checked);
+        }
+        
+    }
+}
